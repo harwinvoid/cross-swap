a93b9efd45f7a595d1fbe338e5da33beb0701d62
yanghuayun <yanghuayun@gumingnc.com>
2021-07-20 22:53:51 +0800

    feat(init): init project

---
+++ src/components/CurrencyLogo/index.tsx
@@ -0,0 +1,65 @@
+/*
+ * @Author: yanghuayun
+ * @Date: 2021-07-13 21:07:22
+ * @LastEditors: yanghuayun
+ * @LastEditTime: 2021-07-19 01:26:17
+ * @Description: file content
+ */
+import { Currency } from '@uniswap/sdk-core'
+import React, { useMemo } from 'react'
+import styled from 'styled-components/macro'
+import useHttpLocations from '../../hooks/useHttpLocations'
+import { WrappedTokenInfo } from '../../state/lists/wrappedTokenInfo'
+import Logo from '../Logo'
+
+const FantomLogo = 'https://pbs.twimg.com/profile_images/1292932673384714241/_BSnTr5s_400x400.png'
+
+export const getTokenLogoURL = (address: string) =>
+  `https://raw.githubusercontent.com/uniswap/assets/master/blockchains/ethereum/assets/${address}/logo.png`
+
+const StyledFantomLogo = styled.img<{ size: string }>`
+  width: ${({ size }) => size};
+  height: ${({ size }) => size};
+  box-shadow: 0px 6px 10px rgba(0, 0, 0, 0.075);
+  border-radius: 24px;
+`
+
+const StyledLogo = styled(Logo)<{ size: string }>`
+  width: ${({ size }) => size};
+  height: ${({ size }) => size};
+  border-radius: ${({ size }) => size};
+  box-shadow: 0px 6px 10px rgba(0, 0, 0, 0.075);
+  background-color: ${({ theme }) => theme.white};
+`
+
+export default function CurrencyLogo({
+  currency,
+  size = '24px',
+  style,
+  ...rest
+}: {
+  currency?: Currency
+  size?: string
+  style?: React.CSSProperties
+}) {
+  const uriLocations = useHttpLocations(currency instanceof WrappedTokenInfo ? currency.logoURI : undefined)
+
+  const srcs: string[] = useMemo(() => {
+    if (!currency || currency.isNative) return []
+
+    if (currency.isToken) {
+      const defaultUrls = currency.chainId === 250 ? [getTokenLogoURL(currency.address)] : []
+      if (currency instanceof WrappedTokenInfo) {
+        return [...uriLocations, ...defaultUrls]
+      }
+      return defaultUrls
+    }
+    return []
+  }, [currency, uriLocations])
+
+  if (currency?.isNative) {
+    return <StyledFantomLogo src={FantomLogo} size={size} style={style} {...rest} />
+  }
+
+  return <StyledLogo size={size} srcs={srcs} alt={`${currency?.symbol ?? 'token'} logo`} style={style} {...rest} />
+}
---
+++ src/entities/fantom.ts
@@ -0,0 +1,37 @@
+/*
+ * @Author: yanghuayun
+ * @Date: 2021-07-19 00:47:27
+ * @LastEditors: yanghuayun
+ * @LastEditTime: 2021-07-20 22:34:23
+ * @Description: file content
+ */
+import invariant from 'tiny-invariant'
+import { Currency, NativeCurrency, Token } from '@uniswap/sdk-core'
+
+export const WFTM = new Token(250, '0x21be370d5312f44cb42ce377bc9b8a0cef1a4c83', 18, 'WFTM', 'Wrapped FTM')
+
+/**
+ * Fantom is the main usage of a 'native' currency, i.e. for Ethereum mainnet and all testnets
+ */
+export class Fantom extends NativeCurrency {
+  protected constructor(chainId: number) {
+    debugger
+    super(chainId, 18, 'FTM', 'Fantom')
+  }
+
+  public get wrapped(): Token {
+    const wftm = WFTM
+    invariant(!!wftm, 'WRAPPED')
+    return wftm
+  }
+
+  private static _etherCache: { [chainId: number]: Fantom } = {}
+
+  public static onChain(chainId: number): Fantom {
+    return this._etherCache[chainId] ?? (this._etherCache[chainId] = new Fantom(chainId))
+  }
+
+  public equals(other: Currency): boolean {
+    return other.isNative && other.chainId === this.chainId
+  }
+}
